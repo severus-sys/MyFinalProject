08cfe3b9c119ecd385acfba0bf2c53af2d22b79c
severus <talat.sudent@gmail.com>
2023-04-16 02:11:00 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+
+    //SOLID
+    //O -- Open Closed Principle -- yeni bir özellik ekliyorsan mevcutdaki kodlara dokunmamak
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal ());
+
+            foreach (var product in productManager.GetByUnitPrice(50,100))
+            {
+                Console.WriteLine(product.ProductName);
+                
+            }
+
+            
+            
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+
+    //generic constraint
+    //class : referans tip
+    //Ientity : olabilir veya IEntity implemente eden bir nesne olabilir
+    // new ' lenebilir olmalı
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); // tüm data filtre
+        
+        T Get(Expression<Func<T, bool>> filter); 
+        
+        void Add(T entity);
+        
+        void Update(T entity);
+        
+        void Delete(T entity);
+
+        
+    }
+}
